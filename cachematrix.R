4b6a97057eb13820255755b0aa955ff01f1c316b
Mark Yampanis <mjyampa@hotmail.com>
2014-11-12 17:55:55 -0800

    changes to complete programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions calculate and store the inverse of a matrix
+## They can be used to avoid performing the calculation multiple times 
+## on the same matrix
 
-## Write a short comment describing this function
 
+## The makeCacheMatrix() function will store the inverse of a matrix that has been 
+## calculated in the function cacheSolve().  It provides methods to store and 
+## retrieve
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve() function check if inverse has previously been calculated
+## and stored using methods in makeCacheMatrix(), then either retrievs the result
+## if it has been stored, or calculates the inverse and stores it.  
+##In both cases the matrix invers is returned
 
+
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
+
+## Usage Example:
+##> M = matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
+##> m <- makeCacheMatrix(M)
+##> m$getinverse()
+##NULL
+##> cacheSolve(m)
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##> m$getinverse()
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##> cacheSolve(m)
+##getting cached data
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##
+## source("GitHub\\ProgrammingAssignment2\\cachematrix.R")
